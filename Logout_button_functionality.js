53ae3363feaed5135a0641b5543a365bd0e4ff7a
capi2002 <lucacapi55>
2023-06-23 13:05:58 -0700

    Logout button will now direct you back to the homepage if clicked

---
+++ Logout_button_functionality.js
@@ -0,0 +1,8 @@
+// Wait for the document to be fully loaded
+$(document).ready(function() {
+  // Handle click event on logout button
+  $('#logout-button').click(function() {
+    // Redirect to the homepage
+    window.location.href = "Main.HTML";
+  });
+});
