ee8128a6e0e7249535c59ce27b6a34b9d8e4d946
kkakkary <reachk3vin@gmail.com>
2023-06-24 18:16:20 -0700

    front end with back end function in local server

--- ClassGPT/Home/urls.py
+++ ClassGPT/Home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from Home import views
 
 urlpatterns = [
-    path("", views.render_index, name="render_index")
+    path("", views.render_index, name="render_index"),
+    path('menu',views.render_menu,name="render_menu")
 ]
--- ClassGPT/Home/views.py
+++ ClassGPT/Home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def render_index(request):
     return render(request, "Main.html")
+
+def render_menu(request):
+    return render(request,"classgpt.html")
