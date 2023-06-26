5406b277c5f846a7db9e1ddd2844660840ffaf6c
Jacob Anderson <jacob.d.anderson8@gmail.com>
2023-06-26 13:52:51 -0700

    Chatbot is responding, formatting is off.

--- ClassGPT/Home/views.py
+++ ClassGPT/Home/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import openai
+
+
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = message,
+        max_tokens = 150,
+        n = 1,
+        stop = None,
+        temperature = 0.7,
+    )
+    
+    answer = response.choices[0].text.strip()
+    return answer
+
 
 # Create your views here.
 def render_index(request):
     return render(request, "Main.html")
 
 def render_menu(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'message': message, "response": response})
     return render(request,"classgpt.html")
