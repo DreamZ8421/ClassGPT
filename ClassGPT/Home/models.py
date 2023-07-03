8a5629a61cf38bd841ab7f998597fc27fb560440
kkakkary <reachk3vin@gmail.com>
2023-07-02 20:06:09 -0700

    commented the stuff for the account feature

--- ClassGPT/Home/models.py
+++ ClassGPT/Home/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 class Chat(models.Model):
@@ -10,3 +13,19 @@ class Chat(models.Model):
 
     def __str__(self):
         return f'{self.user.username}: {self.message}'
+#class Profile(models.Model):
+    #user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #major = models.TextField(max_length=500, blank=True)
+    #ID = models.IntegerField(max_length=9, blank=True)
+    #graduation_year = models.IntegerField(max_length=4, blank=True)
+    #school = models.TextField(max_lenght=30,blank=True)
+    
+
+#@receiver(post_save, sender=User)
+#def create_user_profile(sender, instance, created, **kwargs):
+    #if created:
+        #Profile.objects.create(user=instance)
+
+#@receiver(post_save, sender=User)
+#def save_user_profile(sender, instance, **kwargs):
+   # instance.profile.save()
